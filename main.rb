8a7c4e0260e6a0f8a4ce3e9e90b85132477a9342
Everesh <everesh@proton.me>
2025-04-16 21:02:22 +0200

    Moved main output from stdout to logger

--- main.rb
+++ main.rb
@@ -3,9 +3,23 @@
 require_relative 'lib/wanikani'
 require_relative 'lib/review'
 
-puts 'Welcome to WaniKani TUI!'
+Wanikani::LOGGER.info('▖  ▖    ▘▖▖    ▘  ▄▖▖▖▄▖')
+Wanikani::LOGGER.info('▌▞▖▌▀▌▛▌▌▙▘▀▌▛▌▌▄▖▐ ▌▌▐ ')
+Wanikani::LOGGER.info('▛ ▝▌█▌▌▌▌▌▌█▌▌▌▌  ▐ ▙▌▟▖ v.0.0.0')
 Wanikani.fetch_assignments
 Wanikani.fetch_all_subjects
 
 review_queue = Review.new
-puts review_queue.next
+Wanikani::LOGGER.info('=============================')
+Wanikani::LOGGER.info("Next item: #{review_queue.next['data']['characters']}")
+Wanikani::LOGGER.info("Readings: #{review_queue.next['data']['readings'].reduce('') do |str, reading|
+  str += ', ' unless str.length.zero?
+  str += reading['reading'] if reading['accepted_answer']
+  str
+end}")
+Wanikani::LOGGER.info("Meanings: #{review_queue.next['data']['meanings'].reduce('') do |str, meaning|
+  str += ', ' unless str.length.zero?
+  str += meaning['meaning'] if meaning['accepted_answer']
+  str
+end}")
+Wanikani::LOGGER.info('=============================')
