5cc9e4452613c5ca7d1118a5cae3b19ad77e8449
Everesh <everesh@proton.me>
2025-04-16 17:28:42 +0200

    Add logging

--- lib/wanikani.rb
+++ lib/wanikani.rb
@@ -4,6 +4,8 @@ require 'net/http'
 require 'json'
 require 'uri'
 require 'dotenv/load'
+require 'fileutils'
+require 'logger'
 
 # Provides methods to fetch and cache data from the WaniKani API.
 module Wanikani
@@ -14,33 +16,52 @@ module Wanikani
   ASSIGNMENTS_CACHE_FILE = File.join(CACHE_PATH, 'assignments.json')
   SUBJECTS_CACHE_FILE = File.join(CACHE_PATH, 'subjects.json')
 
+  LOGGER = Logger.new($stdout) # Temporary STDOUT dump
+  LOGGER.level = Logger::INFO
+
   def self.fetch_assignments(force: false)
     FileUtils.mkdir_p(CACHE_PATH)
-    return if File.exist?(ASSIGNMENTS_CACHE_FILE) && !force
 
+    if File.exist?(ASSIGNMENTS_CACHE_FILE) && !force
+      LOGGER.info('Using cached assignments. No requests made!')
+      return
+    end
+
+    LOGGER.info('Fetching assignments from WaniKani...')
     all_assignments = request_all_data('https://api.wanikani.com/v2/assignments')
 
     File.write(ASSIGNMENTS_CACHE_FILE, JSON.pretty_generate(all_assignments))
+    LOGGER.info("Assignments cached to #{ASSIGNMENTS_CACHE_FILE}")
   end
 
   def self.fetch_all_subjects(force: false)
     FileUtils.mkdir_p(CACHE_PATH)
-    return if File.exist?(SUBJECTS_CACHE_FILE) && !force
 
+    if File.exist?(SUBJECTS_CACHE_FILE) && !force
+      LOGGER.info('Using cached subjects. No requests made!')
+      return
+    end
+
+    LOGGER.info('Fetching subjects from WaniKani...')
     all_subjects = request_all_data('https://api.wanikani.com/v2/subjects')
 
     File.write(SUBJECTS_CACHE_FILE, JSON.pretty_generate(all_subjects))
+    LOGGER.info("Subjects cached to #{SUBJECTS_CACHE_FILE}")
   end
 
   def self.request_all_data(url)
     all_pages = []
+    page = 1
 
     while url
+      LOGGER.debug("Requesting page #{page} - #{url}")
       data = request(url)
       all_pages.concat(data['data'])
       url = data.dig('pages', 'next_url')
+      page += 1
     end
 
+    LOGGER.info("Fetched #{all_pages.size} items in total.")
     all_pages
   end
 
@@ -53,8 +74,12 @@ module Wanikani
       http.request(req)
     end
 
-    raise "Error during GET request: #{response.code}" unless response.is_a?(Net::HTTPSuccess)
+    unless response.is_a?(Net::HTTPSuccess)
+      LOGGER.error("Request failed with code #{response.code}")
+      raise "Error during GET request: #{response.code}"
+    end
 
+    LOGGER.debug("Successful response from #{url}")
     JSON.parse(response.body)
   end
 
--- main.rb
+++ main.rb
@@ -1,9 +1,5 @@
 require_relative 'lib/wanikani'
 
 puts 'Welcome to WaniKani TUI!'
-puts '==> Fetching assignments...'
 Wanikani.fetch_assignments
-puts '==> Done!'
-puts '==> Fetching subjects...'
 Wanikani.fetch_all_subjects
-puts '==> Done!'
