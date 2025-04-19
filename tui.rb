77bfb565300cb1776bf58bb47f48b14659ca52b6
Everesh <everesh@proton.me>
2025-04-19 17:13:52 +0200

    Tie ask-input separator to reading questions

--- tui.rb
+++ tui.rb
@@ -159,13 +159,13 @@ class WaniKaniTUI
     if reading
       @ask.bkgd(Curses.color_pair(4))
       @ask.addstr('Reading:')
+      @ask.setpos(2, 0)
+      @ask.addstr('_' * Curses.cols)
     else
       @ask.bkgd(Curses.color_pair(5))
       @ask.addstr('Meaning:')
     end
     @ask.attroff(Curses::A_BOLD)
-    @ask.setpos(2, 0)
-    @ask.addstr('_' * Curses.cols)
   end
 
   def get_input(reading)
