3133ee5a112b2913a52f621376f9f764af7462a4
Everesh <everesh@proton.me>
2025-06-01 16:06:50 +0200

    Introduce data_normalization util

---
+++ lib/util/data_normalizer.rb
@@ -0,0 +1,74 @@
+# frozen_string_literal: true
+
+module WaniKaniTUI
+  # Takes parsed JSON objects and pre-processes them for purposes of injection into the DB
+  class DataNormalizer
+    # Output presented as
+    ## {
+    ### subjects: [[id, characters, level, object, slug, url, mnemonic_r, mnemonic_m], ...]
+    ### meanings: [[id, meaning, primary, accepted], ...]
+    ### readings: [[id, reading, primary, accepted, type], ...]
+    ### components: [[id_component, id_product], ...]
+    ## }
+    def self.subjects(data)
+      normalized = { subjects: [], meanings: [], readings: [], components: [] }
+
+      data.each do |subject|
+        normalized[:subjects] << extract_subject(subject)
+        normalized[:meanings] += extract_meanings(subject)
+        normalized[:readings] += extract_readings(subject)
+        normalized[:components] += extract_components(subject)
+      end
+
+      normalized
+    end
+
+    # privatized methods of the static class
+    class << self
+      private
+
+      def extract_meanings(subject)
+        subject['meanings'].map { |m| extract_meaning(subject, m) }
+      end
+
+      def extract_readings(subject)
+        return [] unless %w[kanji vocabulary].include?(subject['object'])
+
+        subject['readings'].map { |r| extract_reading(subject, r) }
+      end
+
+      def extract_components(subject)
+        return [] unless subject['object'] == 'kanji'
+
+        subject['component_subject_ids'].map { |radical| radical_kanji(radical, subject) } +
+          subject['amalgamation_subject_ids'].map { |vocab| kanji_vocab(subject, vocab) }
+      end
+
+      def extract_subject(subject)
+        [subject['id'], subject['data']['characters'], subject['data']['level'],
+         subject['object'], subject['data']['slug'], subject['data']['document_url'],
+         subject['data']['reading_mnemonic'], subject['data']['meaning_mnemonic']]
+      end
+
+      # Boolean to int conversion due to sqlite3 boolean representation
+      def extract_meaning(subject, meaning)
+        [subject['id'], meaning['meaning'], meaning['primary'] ? 1 : 0,
+         meaning['accepted_answer'] ? 1 : 0]
+      end
+
+      # Boolean to int conversion due to sqlite3 boolean representation
+      def extract_reading(subject, reading)
+        [subject['id'], reading['reading'], reading['primary'] ? 1 : 0,
+         reading['accepted_answer'] ? 1 : 0, reading['type']]
+      end
+
+      def radical_kanji(radical, kanji)
+        [radical, kanji['id']]
+      end
+
+      def kanji_vocab(kanji, vocab)
+        [kanji['id'], vocab]
+      end
+    end
+  end
+end
