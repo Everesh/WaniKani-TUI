f11ac920224cd7cc50eabb186b624527c296a450
Everesh <everesh@proton.me>
2025-06-01 17:19:09 +0200

    Refactor drop script so it works with PRAGMA fks ON

--- lib/db/drop.sql
+++ lib/db/drop.sql
@@ -1,14 +1,10 @@
-PRAGMA foreign_keys = OFF;
-
-DROP TABLE IF EXISTS subject;
-DROP TABLE IF EXISTS components;
-DROP TABLE IF EXISTS meaning;
-DROP TABLE IF EXISTS subject_meaning;
-DROP TABLE IF EXISTS reading;
-DROP TABLE IF EXISTS subject_reading;
-DROP TABLE IF EXISTS assignment;
 DROP TABLE IF EXISTS review;
 DROP TABLE IF EXISTS lesson;
+DROP TABLE IF EXISTS assignment;
+DROP TABLE IF EXISTS subject_reading;
+DROP TABLE IF EXISTS subject_meaning;
+DROP TABLE IF EXISTS components;
+DROP TABLE IF EXISTS reading;
+DROP TABLE IF EXISTS meaning;
+DROP TABLE IF EXISTS subject;
 DROP TABLE IF EXISTS meta;
-
-PRAGMA foreign_keys = ON;
