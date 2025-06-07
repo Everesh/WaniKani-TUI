97cd82e115e1506469319000bfb53cdb0175778d
Everesh <everesh@proton.me>
2025-06-07 19:31:08 +0200

    Remove unnecessary sleeps

--- tui/main.rb
+++ tui/main.rb
@@ -24,14 +24,14 @@ module WaniKaniTUI
         @layout << TitleScreen.new(@main, @preferences, @cjk_renderer)
 
         @engine = init_engine
-        sleep(2)
+
+        sleep(10)
       end
 
       private
 
       def init_engine(force_db_regen: false, api_key: nil)
         @status_line.status('Initializing the engine...')
-        sleep(2)
         Engine.new(force_db_regen: force_db_regen, api_key: api_key)
         @status_line.clear
       rescue SchemaCorruptedError
@@ -41,7 +41,6 @@ module WaniKaniTUI
         @status_line.state('API key not set!')
         api_key = @status_line.win.getch
         @status_line.state("Captured #{api_key}!")
-        sleep(2)
         init_engine(api_key: api_key, force_db_regen: force_db_regen)
       end
 
