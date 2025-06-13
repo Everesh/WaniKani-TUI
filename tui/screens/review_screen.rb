c379cbc64d930137a267ae08d05ed2f27d5f27e7
Everesh <everesh@proton.me>
2025-06-13 19:13:35 +0200

    Add basic review boilerplate

--- tui/screens/review_screen.rb
+++ tui/screens/review_screen.rb
@@ -8,7 +8,8 @@ module WaniKaniTUI
 
       def initialize(main)
         @main = main
-        @win = Curses::Window.new(Curses.lines - 1, Curses.cols, 0, 0)
+        @last_line = Curses.lines - 2
+        @win = Curses::Window.new(@last_line + 1, Curses.cols, 0, 0)
       end
 
       def open
@@ -29,7 +30,55 @@ module WaniKaniTUI
       def draw
         @win.keypad(true) # Refocuses keypad to avoid misscapture from main_menu
         @win.clear
+
+        draw_chars
+        draw_answer_box
+        draw_progress_bar
+
         @win.refresh
+      end
+
+      def draw_chars
+        chars = @main.engine.get_review[:subject]['characters']
+        chars = @main.engine.get_review[:subject]['slug'] if chars.nil?
+        zero_gap = @main.preferences['no_line_spacing_correction']
+
+        if chars.length > 2
+          width = (Curses.cols * 2) / 3
+          subject = @main.cjk_renderer.get_braille(chars, width, zero_gap: zero_gap, size_as_width: true)
+        else
+          height = Curses.lines / 2
+          subject = @main.cjk_renderer.get_braille(chars, height, zero_gap: zero_gap)
+        end
+
+        top_offset = Curses.lines / 6
+        subject.each_with_index do |row, i|
+          @win.setpos(top_offset + i, ((Curses.cols - row.length) / 2) + 1)
+          @win.addstr(row.join(''))
+        end
+      end
+
+      def draw_answer_box
+        @win.setpos(@last_line - 6, 0)
+        @win.addstr('_' * Curses.cols)
+
+        @win.setpos(@last_line - 3, 0)
+        @win.addstr('_' * Curses.cols)
+
+        @win.setpos(@last_line, 0)
+        @win.addstr('_' * Curses.cols)
+      end
+
+      def draw_progress_bar
+        @win.setpos(0, 0)
+        last_col = (@main.engine.progress_statuss_reviews * Curses.cols).floor
+        @win.addstr('█' * last_col)
+        @win.setpos(0, last_col)
+        @win.addstr('░' * (Curses.cols - last_col))
+
+        progress_string = "#{@main.engine.common_query.count_pending_review_reports}/#{@main.engine.common_query.count_available_reviews}"
+        @win.setpos(1, Curses.cols - 1 - progress_string.length)
+        @win.addstr(progress_string)
       end
     end
   end
